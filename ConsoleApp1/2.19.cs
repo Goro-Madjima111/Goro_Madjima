db16f3f401dccf814c99116575fae91ed89f3472
Goro-Madjima111 <a.karagadian2001@gmail.com>
2023-09-05 00:57:33 +0300

    Добавление 2.19

---
+++ ConsoleApp1/2.19.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите c: ");
+double c = double.Parse(Console.ReadLine());
+Console.Write("Введите d: ");
+double d = double.Parse(Console.ReadLine());
+Console.WriteLine(a+b+c+d);
+Console.WriteLine(a*b*c*d);
--- ConsoleApp1/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.Write("Введите m: ");
-double m = double.Parse(Console.ReadLine());
-Console.WriteLine($"N= {m * m + 2.8 * Math.Abs(m) + 0.55}");
