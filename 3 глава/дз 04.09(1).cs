b8f9b98d838e2ff5e8e28358be026e71b893d247
Goro-Madjima111 <a.karagadian2001@gmail.com>
2023-09-05 23:38:23 +0300

    4

---
+++ 3 глава/дз 04.09(1).cs
@@ -0,0 +1,34 @@
+﻿//миллиметр в сантиметры
+//const float sm = 0.1f;
+//Console.Write("Введите миллиметры:");
+//double mm = double.Parse(Console.ReadLine());
+//double res = sm * mm;
+//Console.WriteLine($"{mm} миллиметры {res:F2} сантиметров");
+
+//миллиметр в метры
+//const float m = 0.001f;
+//Console.Write("Введите миллиметры:");
+//double mm = double.Parse(Console.ReadLine());
+//double res = m * mm;
+//Console.WriteLine($"{mm} миллиметры {res:F2} метров");
+
+//миллиметр в км
+//const float km = 0.000001f;
+//Console.Write("Введите миллиметры:");
+//double mm = double.Parse(Console.ReadLine());
+//double res = km * mm;
+//Console.WriteLine($"{mm} миллиметры {res:F5} km");
+
+//миллиметр в мили
+//Console.Write("Введите миллиметры:");
+//double mm = double.Parse(Console.ReadLine());
+//double res = mm / 1609344;
+//Console.WriteLine($"{mm} миллиметры {res:F9} мили");
+
+//миллиметр в ярды
+const float yard = 0.0011f;
+Console.Write("Введите миллиметры:");
+double mm = double.Parse(Console.ReadLine());
+double res = yard * mm;
+Console.WriteLine($"{mm} миллиметры {res:F4} ярдов");
+
