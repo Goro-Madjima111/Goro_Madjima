5fec015b9bdd04c0a8e6693c0bb57f88322f8017
Goro-Madjima111 <a.karagadian2001@gmail.com>
2023-09-15 01:31:33 +0300

    Добавление дз 11.09

---
+++ дз 11.09/дз 11.09.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, запрашивающую у пользователя радиус 
+//окружности и производящую просчет площади круга, используя 
+//заранее заданную константу числа PI.
+const double n = Math.PI;
+Console.Write("Введите радиус окружности:");
+double r = double.Parse(Console.ReadLine());
+double s = n * r * r;
+Console.WriteLine($"Площадь круга:{s:F3}");
+
+//Напишите программу, запрашивающую у пользователя данные о 
+//нем и выводящую его досье. 
+//Используйте для данного задания Интерполяцию. 
+Console.Write("Введите ваше имя: ");
+string name  = Console.ReadLine();
+Console.Write("Введите ваш возраст: ");
+int age = int.Parse(Console.ReadLine());
+Console.Write("Введите ваш город: ");
+string city  = Console.ReadLine();
+
+Console.WriteLine($"Имя: {name}");
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"Город: {city}");
