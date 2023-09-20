c05b2975b1eef506cd3fe231fdd35f0778e867dc
Goro-Madjima111 <a.karagadian2001@gmail.com>
2023-09-20 11:39:07 +0300

    Добавление 18.09

---
+++ 18.09/18.09.cs
@@ -0,0 +1,13 @@
+﻿ //1
+int Petya = 10;
+Console.WriteLine($"Петя взял {Petya} книг");
+int Alesha = Petya - 4;
+Console.WriteLine($"Алеша взял {Alesha} книг");
+int Vanya = Alesha + 2;
+Console.WriteLine($"Ваня взял {Vanya} книг");
+Console.WriteLine($"Сумма всех книг: {Petya + Alesha + Vanya}");
+
+//2
+int FirstBasket = 23;
+int SecondBasket = FirstBasket - 11;
+Console.WriteLine($"Яблок в обеих корзинах: {FirstBasket + SecondBasket}");
