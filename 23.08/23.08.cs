5a4fefc1e05bf6136c1c6cebe525f46286b2c2ef
Goro-Madjima111 <a.karagadian2001@gmail.com>
2023-09-20 11:18:17 +0300

    Добавление 23.08

---
+++ 23.08/23.08.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine($"Two roads diverged in a yellow wood,\nAnd sorry I could not travel both\nAnd be one traveler, long I stood\nAnd looked down one as far as I could\nTo where it bent in the undergrowth.");
+
+byte firstNumber = 1;
+byte secondNumber = 13;
+byte thirdNumber = 49;
+Console.WriteLine($"{firstNumber} {secondNumber} {thirdNumber}");
+
+byte fourthNumber = 21;
+byte fifteenNumber = 35;
+byte sixteenNumber = 80;
+Console.WriteLine($"{fourthNumber}  {fifteenNumber}  {sixteenNumber}");
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,3 @@
+﻿string FirstOffers = "Добиться успеха легко";
+string SecondOffers = "познать смысл - вот проблема";
+Console.WriteLine($"{FirstOffers}, {SecondOffers}");
